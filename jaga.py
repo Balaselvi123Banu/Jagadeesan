f858799623a2fdd9aad41a4b238559f049bcc0f5
Balaselvi123Banu <49433208+Balaselvi123Banu@users.noreply.github.com>
2019-05-02 15:19:39 +0530

    balaselvi is a cute girl commit

--- jaga.py
+++ jaga.py
@@ -1,5 +1,7 @@
 print("Hi I am jagadeesan")
 print("Hi sindhu ")
-print("Sindhu is a bad girl")
+print("Sindhu is a good girl")
 
 print("Durga is a smart girl")
+
+print("Balaselvi is a cute girl")
